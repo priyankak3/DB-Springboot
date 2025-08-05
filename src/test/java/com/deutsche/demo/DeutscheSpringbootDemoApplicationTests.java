baa10aa1d5983c5ecd2f33b5e3af7d10428e904b
priyankak3 <itspriyanka.kadam@spit.ac.in>
2025-08-05 12:42:59 +0530

    test

--- src/test/java/com/deutsche/demo/DeutscheSpringbootDemoApplicationTests.java
+++ src/test/java/com/deutsche/demo/DeutscheSpringbootDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeutscheSpringbootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
